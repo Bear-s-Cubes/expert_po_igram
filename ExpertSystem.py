597f838524acc5197c563a64c7cef8f536121d86
Dill Dough <kuzin.ilya0208@gmail.com>
2024-03-14 13:11:28 +0700

    baza est' pora rabotat'

---
+++ ExpertSystem.py
@@ -0,0 +1,5 @@
+from experta import *
+
+class ExperSystem(KnowledgeBase):
+    def __init__(self):
+        super().__init__()
---
+++ main.py
@@ -0,0 +1,12 @@
+import ExpertSystem
+
+
+
+def main():
+    ES = ExpertSystem()
+    ES.reset()
+    return 0
+
+if __name__ == '__main__':
+    main()
+    
