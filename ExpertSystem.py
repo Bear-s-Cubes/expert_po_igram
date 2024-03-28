22f4796d5066555e1a2c82cfaf1f7e0cf9be8ac8
Bear-Creator <fedorishchev46@gmail.com>
2024-03-28 13:10:57 +0700

    renamed some defs

--- ExpertSystem.py
+++ ExpertSystem.py
@@ -4,18 +4,18 @@ class ExperSystem(KnowledgeEngine):
     def __init__(self):
         super().__init__()
         self.trust_scores = {
-            "Dota2": 0,
-            "Minecraft": 0,
-            "Factorio": 0,
-            "Poppy": 0,
-            "WoT": 0,
+            "Dota2": 0.0,
+            "Minecraft": 0.0,
+            "Factorio": 0.0,
+            "Poppy": 0.0,
+            "WoT": 0.0,
         }
         self.distrust_scores = {
-            "Dota2": 0,
-            "Minecraft": 0,
-            "Factorio": 0,
-            "Poppy": 0,
-            "WoT": 0,
+            "Dota2": 0.0,
+            "Minecraft": 0.0,
+            "Factorio": 0.0,
+            "Poppy": 0.0,
+            "WoT": 0.0,
         }
     
     def addTrust(self, game: str, multiplier: float):
@@ -58,33 +58,33 @@ class ExperSystem(KnowledgeEngine):
         
     @Rule(Fact(answ="5-1") | Fact(answ="6-1") | Fact(answ = "7 - 2") | Fact(answ = "13 - 1"))
     def handle_Singler(self):
-        self.updTrust("Poppy", 0.4)
-        self.updTrust("Minecraft", 0.3)
-        self.updTrust("Factorio", 0.4)
-        self.updTrust("WoT", 0.1)
-        self.updDistrust("Dota2", 0.4)
+        self.addTrust("Poppy", 0.4)
+        self.addTrust("Minecraft", 0.3)
+        self.addTrust("Factorio", 0.4)
+        self.addTrust("WoT", 0.1)
+        self.addDistrust("Dota2", 0.4)
     
     @Rule(Fact(answ="5-2") | Fact(answ="6-2") | Fact(answ = "7 - 3") | Fact(answ = "13 - 2"))
     def handle_Coop(self):
-        self.updDistrust("Poppy", 0.2)
-        self.updTrust("Minecraft", 0.4)
-        self.updTrust("Factorio", 0.2)
-        self.updTrust("WoT", 0.3)
-        self.updTrust("Dota2", 0.4)
+        self.addDistrust("Poppy", 0.2)
+        self.addTrust("Minecraft", 0.4)
+        self.addTrust("Factorio", 0.2)
+        self.addTrust("WoT", 0.3)
+        self.addTrust("Dota2", 0.4)
     
     @Rule(Fact(answ="5-3") | Fact(answ="6-3") | Fact(answ = "7 - 1") | Fact(answ = "13 - 3"))
     def handle_Multi(self):
-        self.updDistrust("Poppy", 0.4)
-        self.updTrust("Minecraft", 0.2)
-        self.updDistrust("Factorio", 0.2)
-        self.updTrust("WoT", 0.5)
-        self.updTrust("Dota2", 0.5)
+        self.addDistrust("Poppy", 0.4)
+        self.addTrust("Minecraft", 0.2)
+        self.addDistrust("Factorio", 0.2)
+        self.addTrust("WoT", 0.5)
+        self.addTrust("Dota2", 0.5)
         
     @Rule(Fact(answ="1-2") | Fact(answ="12 - 3") | Fact(answ = "11 - 2") | Fact(answ = "8 - 2") | Fact(answ = "9 - 1"))
     def handle_TrueTryhard(self):
-        self.updTrust("Factorio", 0.4)
-        self.updTrust("WoT", 0.3)
-        self.updTrust("Dota2", 0.5)
-        self.updDistrust("Minecraft", 0.1)
-        self.updDistrust("Poppy", 0.4)
+        self.addTrust("Factorio", 0.4)
+        self.addTrust("WoT", 0.3)
+        self.addTrust("Dota2", 0.5)
+        self.addDistrust("Minecraft", 0.1)
+        self.addDistrust("Poppy", 0.4)
         
