e6523b467358f5443f0b8cd3dbd790c6390bef2f
ThetaIX <kuzin.ilya0208@gmail.com>
2024-04-04 13:59:33 +0700

    1.1

--- ExpertSystem.py
+++ ExpertSystem.py
@@ -167,7 +167,7 @@ class ExpertSystem(KnowledgeEngine):
           Fact(answ = "11-3"))
     def handle_Horror(self):
         self.addTrust("Poppy", 0.5)
-        self.addTrust("Minecrft", 0.1)
+        self.addTrust("Minecraft", 0.1)
         self.addDistrust("WoT", 0.4)
         self.addDistrust("Dota2", 0.4)
         self.addDistrust("Factorio", 0.4)
